53732c60b577737505ff51b16f92c82c2433ead5
shanrajan <116018266+shanrajan@users.noreply.github.com>
2024-01-20 12:14:59 +0530

    Delete current_time.py

--- current_time.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# Get the current system time with microseconds
-system_time = datetime.datetime.now()
-
-# Print the microseconds part
-microseconds_part = system_time.microsecond
-print(f"Microseconds: {microseconds_part}")
-
